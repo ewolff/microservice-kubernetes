0f1a7e74c79fbe0edea12ed13034ae0bf8cb782f
Eberhard Wolff <eberhard.wolff@gmail.com>
2017-07-08 09:36:37 +0200

    Remove some more left overs

--- microservice-kubernetes-demo/microservice-kubernetes-demo-order/src/main/java/com/ewolff/microservice/order/OrderApp.java
+++ microservice-kubernetes-demo/microservice-kubernetes-demo-order/src/main/java/com/ewolff/microservice/order/OrderApp.java
@@ -3,10 +3,8 @@ package com.ewolff.microservice.order;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
-@EnableDiscoveryClient
 @EnableCircuitBreaker
 public class OrderApp {
 
--- microservice-kubernetes-demo/microservice-kubernetes-demo-order/src/test/java/com/ewolff/microservice/order/OrderTestApp.java
+++ microservice-kubernetes-demo/microservice-kubernetes-demo-order/src/test/java/com/ewolff/microservice/order/OrderTestApp.java
@@ -2,12 +2,8 @@ package com.ewolff.microservice.order;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
-@EnableDiscoveryClient
-@EnableCircuitBreaker
 public class OrderTestApp {
 
 	public static void main(String[] args) {
